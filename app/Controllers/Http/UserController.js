503ac738b12850c497b22ac3045d0c38b66461cd
GenryMarquez <magcinz@hotmail.com>
2020-05-15 15:41:47 -0700

    Commit Inicial Incluido el cambios en routes

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,15 @@
+'use strict'
+
+class UserController {
+
+    store() {
+
+        return {
+            menssage: 'Registro de Usuarios desde el Controlador'
+        }
+
+    }
+
+}
+
+module.exports = UserController
---
+++ start/routes.js
@@ -0,0 +1,35 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URLs and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.get('/', () => {
+    return { greeting: 'Hello world in JSON' }
+})
+
+/*Route.post('Users/register', () => {
+    return { menssage: 'Registro de Usuarios' }
+})*/
+
+//Route.post('Users/register', 'UserController.store');
+
+Route.group(() => {
+    Route.post('Users/register', 'UserController.store');
+}).prefix('Api/v1');
+
+Route.group(() => {
+    Route.post('Users/register', 'UserController.store');
+}).prefix('Api/v2');
